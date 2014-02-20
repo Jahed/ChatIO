960a793a745c8853f05a40a827f22a40d1bb3c4c
Jahed Ahmed <jahed.public@gmail.com>
2014-02-20 01:12:43 +0000

    Added MIT license clauses to look good

--- io/jahed/twitchrobot/TwitchRobot.java
+++ io/jahed/twitchrobot/TwitchRobot.java
@@ -1,4 +1,26 @@
+/**
+ *  Copyright (C) 2014 Jahed Ahmed
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in the
+ *  Software without restriction, including without limitation the rights to use, copy,
+ *  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ *  and to permit persons to whom the Software is furnished to do so, subject to the
+ *  following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all copies
+ *  or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ *  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ *  OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package io.jahed.twitchrobot;
+
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
@@ -13,7 +35,7 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class TwitchRobot implements HttpHandler {
-	
+    
     public static void main(String[] args) throws InterruptedException, IOException, AWTException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8090), 0);
         TwitchRobot twitchRobot = new TwitchRobot();
@@ -34,36 +56,36 @@ public class TwitchRobot implements HttpHandler {
     private Robot robot;
     
     public TwitchRobot() throws AWTException {
-    	allowedChoices = Arrays.asList("up", "down", "left", "right", "a", "b", "x", "y", "l", "r", "start", "select");
-    	robot = new Robot();
-    	robot.setAutoDelay(100); //required to register
-//    	robot.setAutoWaitForIdle(false);
+        allowedChoices = Arrays.asList("up", "down", "left", "right", "a", "b", "x", "y", "l", "r", "start", "select");
+        robot = new Robot();
+        robot.setAutoDelay(100); //required to register
+//        robot.setAutoWaitForIdle(false);
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-    	
+        
         if(!exchange.getRequestMethod().equalsIgnoreCase("POST")) {
-        	return;
+            return;
         }
         
         try(Scanner scanner = new Scanner(exchange.getRequestBody())) {
-        	if(!scanner.hasNext()) return;
-        	
-    		String token = scanner.next();
-    		
-    		if(token.charAt(1) == '»') { //for continuing lines
-    			if(!scanner.hasNext()) {
-    				System.out.println(" xx Rejected (" + token + ")");
-    				return;
-    			}
-    			
-				token = scanner.next();
-    		}
+            if(!scanner.hasNext()) return;
+            
+            String token = scanner.next();
+            
+            if(token.charAt(1) == '»') { //for continuing lines
+                if(!scanner.hasNext()) {
+                    System.out.println(" xx Rejected (" + token + ")");
+                    return;
+                }
+                
+                token = scanner.next();
+            }
 
-        	System.out.println("Message from a User {");        	
-    		perform(token); //only allow first line, first word
-        	System.out.println("}\n");
+            System.out.println("Message from a User {");            
+            perform(token); //only allow first line, first word
+            System.out.println("}\n");
         }
         
         exchange.getResponseBody().close();
@@ -71,65 +93,65 @@ public class TwitchRobot implements HttpHandler {
     }
     
     public void perform(String choice) {
-    	
-    	if(!allowedChoices.contains(choice)) {
-    		System.err.println("  xx Choice Rejected (" + choice + ")");
-    		return;
-    	}
-    	
-    	switch(choice) {
-    		case "up":
-    			robot.keyPress(KeyEvent.VK_NUMPAD8);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD8);
-    			break;
-    		case "down":
-    			robot.keyPress(KeyEvent.VK_NUMPAD2);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD2);
-    			break;
-    		case "left":
-    			robot.keyPress(KeyEvent.VK_NUMPAD4);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD4);
-    			break;
-    		case "right":
-    			robot.keyPress(KeyEvent.VK_NUMPAD6);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD6);
-    			break;
-    		case "a":
-    			robot.keyPress(KeyEvent.VK_NUMPAD3);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD3);
-    			break;
-    		case "b":
-    			robot.keyPress(KeyEvent.VK_NUMPAD1);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD1);
-    			break;
-    		case "x":
-    			robot.keyPress(KeyEvent.VK_NUMPAD9);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD9);
-    			break;
-    		case "y":
-    			robot.keyPress(KeyEvent.VK_NUMPAD7);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD7);
-    			break;
-    		case "l":
-    			robot.keyPress(KeyEvent.VK_SLASH);
-    			robot.keyRelease(KeyEvent.VK_SLASH);
-    			break;
-    		case "r":
-    			robot.keyPress(KeyEvent.VK_ASTERISK);
-    			robot.keyRelease(KeyEvent.VK_ASTERISK);
-    			break;
-    		case "start":
-    			robot.keyPress(KeyEvent.VK_NUMPAD0);
-    			robot.keyRelease(KeyEvent.VK_NUMPAD0);
-    			break;
-    		case "select":
-    			robot.keyPress(KeyEvent.VK_PERIOD);
-    			robot.keyRelease(KeyEvent.VK_PERIOD);
-    			break;
-    		default: return;
-    	}
-    	
-		System.out.println("  -- Choice Performed (" + choice + ")");
+        
+        if(!allowedChoices.contains(choice)) {
+            System.err.println("  xx Choice Rejected (" + choice + ")");
+            return;
+        }
+        
+        switch(choice) {
+            case "up":
+                robot.keyPress(KeyEvent.VK_NUMPAD8);
+                robot.keyRelease(KeyEvent.VK_NUMPAD8);
+                break;
+            case "down":
+                robot.keyPress(KeyEvent.VK_NUMPAD2);
+                robot.keyRelease(KeyEvent.VK_NUMPAD2);
+                break;
+            case "left":
+                robot.keyPress(KeyEvent.VK_NUMPAD4);
+                robot.keyRelease(KeyEvent.VK_NUMPAD4);
+                break;
+            case "right":
+                robot.keyPress(KeyEvent.VK_NUMPAD6);
+                robot.keyRelease(KeyEvent.VK_NUMPAD6);
+                break;
+            case "a":
+                robot.keyPress(KeyEvent.VK_NUMPAD3);
+                robot.keyRelease(KeyEvent.VK_NUMPAD3);
+                break;
+            case "b":
+                robot.keyPress(KeyEvent.VK_NUMPAD1);
+                robot.keyRelease(KeyEvent.VK_NUMPAD1);
+                break;
+            case "x":
+                robot.keyPress(KeyEvent.VK_NUMPAD9);
+                robot.keyRelease(KeyEvent.VK_NUMPAD9);
+                break;
+            case "y":
+                robot.keyPress(KeyEvent.VK_NUMPAD7);
+                robot.keyRelease(KeyEvent.VK_NUMPAD7);
+                break;
+            case "l":
+                robot.keyPress(KeyEvent.VK_SLASH);
+                robot.keyRelease(KeyEvent.VK_SLASH);
+                break;
+            case "r":
+                robot.keyPress(KeyEvent.VK_ASTERISK);
+                robot.keyRelease(KeyEvent.VK_ASTERISK);
+                break;
+            case "start":
+                robot.keyPress(KeyEvent.VK_NUMPAD0);
+                robot.keyRelease(KeyEvent.VK_NUMPAD0);
+                break;
+            case "select":
+                robot.keyPress(KeyEvent.VK_PERIOD);
+                robot.keyRelease(KeyEvent.VK_PERIOD);
+                break;
+            default: return;
+        }
+        
+        System.out.println("  -- Choice Performed (" + choice + ")");
 
     }
 }
--- message-sender.js
+++ message-sender.js
@@ -1,44 +1,65 @@
 // ==UserScript==
-// @name		Twitch Robot Message Sender
-// @author		Jahed
-// @icon		http://i.imgur.com/Lkl0EDA.png
-// @description		For the Twitch Robot to get messages from chat.
-// @version		0.1
-// @date		2014-02-18
-// @updateURL		http://userscripts.org/scripts/source/???
-// @namespace		TwitchRobotMessageSender_Jahed
-// @include		*.twitch.tv/*
+// @name        Twitch Robot Message Sender
+// @author      Jahed
+// @icon        http://i.imgur.com/Lkl0EDA.png
+// @description For the Twitch Robot to get messages from chat.
+// @version     0.1
+// @date        2014-02-18
+// @updateURL   http://userscripts.org/scripts/source/???
+// @namespace   TwitchRobotMessageSender_Jahed
+// @include     *.twitch.tv/*
 // ==/UserScript==
 
+/**
+ *  Copyright (C) 2014 Jahed Ahmed
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *  this software and associated documentation files (the "Software"), to deal in the
+ *  Software without restriction, including without limitation the rights to use, copy,
+ *  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ *  and to permit persons to whom the Software is furnished to do so, subject to the
+ *  following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all copies
+ *  or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ *  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ *  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ *  OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+ 
 var chatLineList = document.getElementById('chat_line_list');
 chatLineList.addEventListener('DOMNodeInserted', setup, false);
 
 function setup(event) {
-	if(event.target.firstElementChild.innerHTML.indexOf('Welcome to the chat room!') === -1) {
-		return;
-	}
+    if(event.target.firstElementChild.innerHTML.indexOf('Welcome to the chat room!') === -1) {
+        return;
+    }
 
-	chatLineList.removeEventListener("DOMNodeInserted", setup, false);
-	chatLineList.addEventListener("DOMNodeInserted", chatAdded, false);
-	
-	console.log('Chat Ready');
+    chatLineList.removeEventListener("DOMNodeInserted", setup, false);
+    chatLineList.addEventListener("DOMNodeInserted", chatAdded, false);
+    
+    console.log('Chat Ready');
 }
 
 function chatAdded(event) {
-	var chatLineElement = event.target,
-		chatText;
-		
-	if(chatLineElement.className !== 'chat_line') {
-		chatLineElement = chatLineElement.getElementsByClassName('chat_line')[0];
-	}
-	
-	chatText = chatLineElement.innerHTML;
-	
-	console.log(chatText);
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open("post", "http://localhost:8090/recieve", true);
-	xhr.send(chatText);
+    var chatLineElement = event.target,
+        chatText;
+        
+    if(chatLineElement.className !== 'chat_line') {
+        chatLineElement = chatLineElement.getElementsByClassName('chat_line')[0];
+    }
+    
+    chatText = chatLineElement.innerHTML;
+    
+    console.log(chatText);
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("post", "http://localhost:8090/recieve", true);
+    xhr.send(chatText);
 }
 
 console.log('Twitch Robot Message Sender Loaded');
