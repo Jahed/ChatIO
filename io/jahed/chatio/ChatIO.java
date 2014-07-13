025e8bcf75e4f497a91874d31ae23171907a0313
Jahed Ahmed <jahed.public@gmail.com>
2014-07-13 15:36:48 +0100

    Moved main method to seperate class

---
+++ io/jahed/chatio/ChatIO.java
@@ -0,0 +1,30 @@
+package io.jahed.chatio;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ChatIO {
+	public static void main(String[] args) throws Exception {
+		//Temporary while there's no error-checking
+		System.out.println("\nIncase something goes wrong, to run the program properly use:\n" +
+				"\tjava -jar chat-io.jar \"config_path.json\" \"keys_path.json\" \n");
+
+		ObjectMapper mapper = new ObjectMapper();
+		
+		ChatConfig config = mapper.readValue(new File(args[0]), ChatConfig.class);
+		
+	    TypeReference<HashMap<String,String>> hashMapTypeRef = new TypeReference<HashMap<String,String> >() {}; 
+	    Map<String,String> keyMap = mapper.readValue(new File(args[1]), hashMapTypeRef); 
+	    
+		ChatRobot chatRobot = new ChatRobot(config, keyMap);
+		
+		System.out.println("\nStarting robot in 5 seconds, focus your window...");
+		Thread.sleep(5000);
+		System.out.println("\nRobot Started | press CTRL + C to stop\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+		chatRobot.run();
+	}
+}
--- io/jahed/chatio/ChatRobot.java
+++ io/jahed/chatio/ChatRobot.java
@@ -48,26 +48,6 @@ public class ChatRobot implements ActionListener {
 	
 	public static final String HOLD_TOKEN = "hold ";
 	public static final int HOLD_TOKEN_LENGTH = HOLD_TOKEN.length();
-	
-	public static void main(String[] args) throws Exception {
-		//Temporary while there's no error-checking
-		System.out.println("\nIncase something goes wrong, to run the program properly use:\n" +
-				"\tjava -jar twitch-robot.jar \"config_path.json\" \"keys_path.json\" \n");
-
-		ObjectMapper mapper = new ObjectMapper();
-		
-		ChatConfig config = mapper.readValue(new File(args[0]), ChatConfig.class);
-		
-	    TypeReference<HashMap<String,String>> hashMapTypeRef = new TypeReference<HashMap<String,String> >() {}; 
-	    Map<String,String> keyMap = mapper.readValue(new File(args[1]), hashMapTypeRef); 
-	    
-		ChatRobot twitchRobot = new ChatRobot(config, keyMap);
-		
-		System.out.println("\nStarting robot in 5 seconds, focus your window...");
-		Thread.sleep(5000);
-		System.out.println("\nRobot Started | press CTRL + C to stop\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-		twitchRobot.run();
-	}
 
 	private Robot inputBot;
 	private PircBotX ircBot;
